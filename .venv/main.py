dcc1af05d9908b03b4b65f5b00ee444b8ed08059
bryanCasabuena <bryancasabuenac@gmail.com>
2024-08-21 11:51:08 +0800

    Scrape from the page source of URL

---
+++ .venv/main.py
@@ -0,0 +1,13 @@
+import requests
+import selectorlib
+
+#Scraping from a URL
+URL = "http://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    response = requests.get(url)
+    source = response.text
+    return source
+
+if __name__ == "__main__":
+    print(scrape(URL))
